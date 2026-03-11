c87512c046f1cc538830ddebe42f5e8e4ec2d420
Pedro da Silva Rodrigues <0001083315@senaimgaluno.com.br>
2026-03-11 17:26:41 -0300

    Mudaça em arquivos e adição de códigos

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <div>
+  <StrictMode>
     <App />,
-  </div>,
-  // </StrictMode>,
+  </StrictMode>,
 );
